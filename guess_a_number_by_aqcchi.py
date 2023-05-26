c149cc36e4aec3ef3f81325a6a0866e1196a9f9c
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2023-05-26 18:08:18 +0300

    Update guess_a_number_by_aqcchi.py

--- guess_a_number_by_aqcchi.py
+++ guess_a_number_by_aqcchi.py
@@ -1,10 +1,11 @@
 # GuessANumberByaqcchi
 import random
-
+number_of_tries = 0
 computer_number = random.randint(1, 100)
 
 while True:
     player_input = input("Guess the number(1, 100): ")
+    number_of_tries += 1
 
     if not player_input.isdigit():
         print("Invalid input! Try again...")
@@ -16,8 +17,14 @@ while True:
         print("You guessed it!")
         break
     elif player_number > computer_number:
+        if number_of_tries > 4:
+            print("No more tries left! GAME OVER!")
+            break
         print("Too High! Try again!")
     else:
+        if number_of_tries > 4:
+            print("No more tries left! GAME OVER!")
+            break
         print("Too Low! Try again!")
 
 
